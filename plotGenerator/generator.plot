f7725bb63cb34b64a3497ff25ffe84e67ace1ad7
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-07-05 16:08:06 -0300

    melhorando e adicionando mais campos nos graficos

--- plotGenerator/generator.plot
+++ plotGenerator/generator.plot
@@ -6,6 +6,6 @@ set size ratio -1 1,1
 set title "Instancia do DVRP (n=250; k=0,15)" 
 set xrange [0:1]
 set yrange [0:1]
-plot 'points.txt' u ($1):($2):($3) with points pt 7 ps variable lc rgb "black", 'edges.txt' with lines lt 1 lw 1 lc rgb "forest-green"
+plot 'vertices.txt' u ($1):($2):($3) with points pt 7 ps variable lc rgb "black", 'edges.txt' with lines lt 0.5 lw 0.5 lc rgb "light-gray", 'solutionEdges.txt' with lines lt 1 lw 1 lc rgb "forest-green", 'solutionVertices.txt' with circles lt 1 lw 0.5 rgb "red"
 
 #plot 'dom_set.txt' with circles lt 1 lw 0.5 lc rgb "red", 'edges.txt' with lines lt 1 lw 2 lc rgb "forest-green", 'points.txt' u ($1):($2) with points pt 7 ps 0.5 lc rgb "black", 'dom_set.txt' u ($1):($2) with points pt 7 lc rgb "red", 'depot.txt' u ($1):($2) with points pt 7 lc rgb "blue"
--- plotGenerator/plotGenerator.cpp
+++ plotGenerator/plotGenerator.cpp
@@ -32,58 +32,94 @@ int main (int argc, char * argv[]) {
     if (inputFile.is_open() && resultFile.is_open()) {
         ulint n, mComplete, m, k;
         inputFile >> n >> mComplete >> m >> k;
-        vector < pair < pair <double, double>, double > > points (n, make_pair(make_pair(0, 0), 0));
+        vector < pair < pair <double, double>, double > > vertices (n, make_pair(make_pair(0, 0), 0));
         cout << "Vertices: " << endl;
+        // reading vertices' coordinates and penalty
         for (ulint v = 0; v < n; v++) {
-            inputFile >> points[v].first.first;
-            inputFile >> points[v].first.second;
-            inputFile >> points[v].second;
-            cout << v << " : (" << points[v].first.first << ", " << points[v].first.second << ")" << endl;
+            inputFile >> vertices[v].first.first;
+            inputFile >> vertices[v].first.second;
+            inputFile >> vertices[v].second;
+            cout << v << " : (" << vertices[v].first.first << ", " << vertices[v].first.second << ")" << endl;
         }
-        vector < pair < pair <double, double>, double > > sortedPoints (points.begin(), points.end());
-        sort(sortedPoints.begin(), sortedPoints.end(), pointComp);
-        double dw = (1.0 - 0.25) / ((double) (n - 1));
-        sortedPoints[0].second = 0.25;
+        // limiting penalities to the interval [0.25, 1]
+        double minPenalty = vertices[0].second;
+        double maxPenalty = vertices[0].second;
         for (int v = 1; v < n; v++) {
-            sortedPoints[v].second = sortedPoints[v - 1].second + dw;
+            if (minPenalty > vertices[v].second) {
+                minPenalty = vertices[v].second;
+            }
+            if (maxPenalty < vertices[v].second) {
+                maxPenalty = vertices[v].second;
+            }
         }
-        ofstream pointsFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/points.txt");
+        for (int v = 0; v < n; v++) {
+            vertices[v].second -= minPenalty;
+        }
+        minPenalty -= minPenalty;
+        maxPenalty -= minPenalty;
+        for (int v = 0; v < n; v++) {
+            vertices[v].second *= (1-0.25)/maxPenalty;
+        }
+        minPenalty *= (1-0.25)/maxPenalty;
+        maxPenalty *= (1-0.25)/maxPenalty;
+        for (int v = 0; v < n; v++) {
+            vertices[v].second += 0.25;
+        }
+
+        // printing vertices' coordinates and penalty
+        ofstream pointsFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/vertices.txt");
         for (ulint v = 0; v < n; v++) {
-            pointsFile << sortedPoints[v].first.first << ' ';
-            pointsFile << sortedPoints[v].first.second << ' ';
-            pointsFile << sortedPoints[v].second << endl;
+            pointsFile << vertices[v].first.first << ' ';
+            pointsFile << vertices[v].first.second << ' ';
+            pointsFile << vertices[v].second << endl;
         }
         pointsFile.close();
+        
+        // reading (and ignoring) the complete graph's edges
+        for (int j = 0; j < mComplete; j++) {
+            int u, v, w;
+            cin >> u >> v >> w;
+        }
+
+        // reading the graph's edges and printing its coordinates
+        ofstream edgesFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/edges.txt");
+        for (int e = 0; e < m; e++) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            edgesFile << vertices[u].first.first << ' ';
+            edgesFile << vertices[u].first.second << endl;
+            edgesFile << vertices[v].first.first << ' ';
+            edgesFile << vertices[v].first.second << endl << endl;
+        }
+        edgesFile.close();
+
         ulint nSolution, mSolution;
         double costSolution;
         resultFile >> nSolution >> mSolution >> costSolution;
-        ofstream domSetFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/dom_set.txt");
+        // reading the solution's vertices and printing its coordinates and neighborhood radio
+        ofstream solutionVerticesFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/solutionVertices.txt");
         for (ulint i = 0; i < nSolution; i++) {
             int v;
             resultFile >> v;
-            domSetFile << points[i].first.first << ' ';
-            domSetFile << points[i].first.second << ' ';
+            domSetFile << vertices[i].first.first << ' ';
+            domSetFile << vertices[i].first.second << ' ';
             domSetFile << ((double) k)/100.0 << endl;
         }
-        vector < pair < pair <double, double>, pair <double, double> > > edgesCoordinates (mSolution, make_pair(make_pair(0.0, 0.0), make_pair(0.0, 0.0)));
+        solutionVerticesFile.close();
+
         cout << "Edges:" << endl;
+        // reading the solution's edges and printing its coordinates
+        ofstream solutionEdgesFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/solutionEdges.txt");
         for (ulint e = 0; e < mSolution; e++) {
             ulint u, v;
             resultFile >> u >> v;
-            edgesCoordinates[e].first.first = points[u].first.first;
-            edgesCoordinates[e].first.second = points[u].first.second;
-            edgesCoordinates[e].second.first = points[v].first.first;
-            edgesCoordinates[e].second.second = points[v].first.second;
-            cout << u << " <-> " << v << " : (" << points[u].first.first << ", " << points[u].first.second << ") <-> (" << points[v].first.first << ", " << points[v].first.second << ")" << endl;
+            cout << u << " <-> " << v << " : (" << vertices[u].first.first << ", " << vertices[u].first.second << ") <-> (" << vertices[v].first.first << ", " << vertices[v].first.second << ")" << endl;
+            solutionEdgesFile << vertices[u].first.first << ' ';
+            solutionEdgesFile << vertices[u].first.second << endl;
+            solutionEdgesFile << vertices[v].first.first << ' ';
+            solutionEdgesFile << vertices[v].first.second << endl << endl;
         }
-        ofstream edgesFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "/edges.txt");
-        for (ulint e = 0; e < mSolution; e++) {
-            edgesFile << edgesCoordinates[e].first.first << ' ';
-            edgesFile << edgesCoordinates[e].first.second << endl;
-            edgesFile << edgesCoordinates[e].second.first << ' ';
-            edgesFile << edgesCoordinates[e].second.second << endl << endl;
-        }
-        edgesFile.close();
+        solutionEdgesFile.close();
     }
     return 0;
 }
