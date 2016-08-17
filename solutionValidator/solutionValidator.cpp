85925b70bf007291504396f9859cfb61619f2743
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-08-17 12:39:40 -0300

    validador

--- solutionValidator/solutionValidator.cpp
+++ solutionValidator/solutionValidator.cpp
@@ -10,27 +10,29 @@ using namespace std;
 typedef long int ulint;
 
 int main (int argc, char * argv[]) {
-    string path, N, D, K, T, R;
+    string path, N, D, K, T, R, P;
 
     cout << argc << endl;
 
-    if (argc == 7) {
+    if (argc == 8) {
         path = string(argv[1]);
         N = string(argv[2]);
         D = string(argv[3]);
         K = string(argv[4]);
         T = string(argv[5]);
         R = string(argv[6]);
+        P = string(argv[7]);
     } else {
         cin >> path >> N >> D >> K >> T >> R;
     }
 
-    ifstream inputFile ("../input/instanceN" + N + "D" + D + "K" + K + "T" + T + "R" + R + ".in");
-    ifstream resultFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "R" + R + "/result.out");
+    ifstream inputFile ("../input/instanceN" + N + "D" + D + "K" + K + "T" + T + "R" + R + "P" + P + ".in");
+    ifstream resultFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "R" + R + "P" + P + "/result.out");
 
     if (inputFile.is_open() && resultFile.is_open()) {
-        ulint n, mComplete, m, k, r;
-        inputFile >> n >> mComplete >> m >> k >> r;
+        ulint n, mComplete, m, k, t, r, root;
+        double d, p;
+        inputFile >> n >> d >> k >> t >> r >> p >> mComplete >> m >> root;
 
         if (N == n) {
             cout << "N == n : OK" << endl;
@@ -50,48 +52,59 @@ int main (int argc, char * argv[]) {
             cout << "R == r : ERROR" << endl;
         }
 
-        vector < pair < pair <double, double>, double > > vertices (n, make_pair(make_pair(0, 0), 0));
-        cout << "Vertices: " << endl;
+        vector < pair <double, double> > verticesCoordinates (n, make_pair(0, 0));
+        vector <double> penalties (n, 0);
         // reading vertices' coordinates and penalty
         for (ulint v = 0; v < n; v++) {
-            inputFile >> vertices[v].first.first;
-            inputFile >> vertices[v].first.second;
+            inputFile >> vertices[v].first;
             inputFile >> vertices[v].second;
+            inputFile >> penalties[v];
         }
 
         // reading (and ignoring) the complete graph's edges
-        for (int j = 0; j < mComplete; j++) {
-            int u, v, w;
+        for (ulint j = 0; j < mComplete; j++) {
+            ulint u, v, w;
             inputFile >> u >> v >> w;
+        }
+
+        vector < pair <double, double> > edges (m, make_pair(0, 0));
+        vector <double> weights (m, 0);
+        // reading the graph's edges and printing its coordinates
+        for (ulint e = 0; e < m; e++) {
+            int u, v, w;
+            inputFile >> edges[e].first;
+            inputFile >> edges[e].second;
+            inputFile >> weights[e];
         }
 
         ulint nSolution, mSolution;
         double costSolution;
         resultFile >> nSolution >> mSolution >> costSolution;
-        // reading the solution's vertices and printing its coordinates and neighborhood radio
-        ofstream solutionVerticesFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "R" + R +"/solutionVertices.txt");
-        for (ulint i = 0; i < nSolution; i++) {
-            int v;
-            resultFile >> v;
-            solutionVerticesFile << vertices[v].first.first << ' ';
-            solutionVerticesFile << vertices[v].first.second << ' ';
-            solutionVerticesFile << ((double) k)/100.0 << endl;
-        }
-        solutionVerticesFile.close();
 
-        cout << "Edges:" << endl;
-        // reading the solution's edges and printing its coordinates
-        ofstream solutionEdgesFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "R" + R + "/solutionEdges.txt");
-        for (ulint e = 0; e < mSolution; e++) {
-            ulint u, v;
-            resultFile >> u >> v;
-            cout << u << " <-> " << v << " : (" << vertices[u].first.first << ", " << vertices[u].first.second << ") <-> (" << vertices[v].first.first << ", " << vertices[v].first.second << ")" << endl;
-            solutionEdgesFile << vertices[u].first.first << ' ';
-            solutionEdgesFile << vertices[u].first.second << endl;
-            solutionEdgesFile << vertices[v].first.first << ' ';
-            solutionEdgesFile << vertices[v].first.second << endl << endl;
+        if (nSolution <= n) {
+            cout << "nSolution <= n : OK" << endl;
+        } else {
+            cout << "nSolution <= n : ERROR" << endl;
         }
-        solutionEdgesFile.close();
+
+        if (mSolution <= m) {
+            cout << "mSolution <= m : OK" << endl;
+        } else {
+            cout << "mSolution <= m : ERROR" << endl;
+        }
+
+        vector <ulint> solutionVertices (nSolution, 0);
+        // reading the solution's vertices
+        for (ulint v = 0; v < nSolution; v++) {
+            resultFile >> solutionVertices[v];
+        }
+
+        vector < pair <ulint, ulint> > solutionEdges (mSolution, make_pair(0, 0));
+        // reading the solution's edges
+        for (ulint e = 0; e < mSolution; e++) {
+            resultFile >> solutionEdges[e].first;
+            resultFile >> solutionEdges[e].second;
+        }
     }
     return 0;
 }
