ae951247dececa4cb4f686ffd03e53b527dad3ef
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-08-17 20:40:24 -0300

    novos parametros k e p

--- instanceGenerator/instanceGenerator.sh
+++ instanceGenerator/instanceGenerator.sh
@@ -6,13 +6,13 @@ for n in 10 20 50 100
 do
     for d in 0.3 0.5 0.7
     do
-        for k in 0 2 4 6 8 10
+        for k in 0 5 10 20
         do
             for t in 0 1
             do
                 for r in 0 1 2
                 do
-                    for p in 0.25 0.5
+                    for p in 0.1 0.5 1.0
                     do
                         echo "N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.};
                         ./instanceGenerator $n $d $k $t $r $p > "../input/instanceN"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}".in";
--- solutionValidator/solutionValidator.sh
+++ solutionValidator/solutionValidator.sh
@@ -5,13 +5,13 @@ for n in 10 20 50 100
 do
     for d in 0.3 0.5 0.7
     do
-        for k in 0 2 4 6 8 10
+        for k in 0 5 10 20
         do
             for t in 0 1
             do
                 for r in 0 1 2
                 do
-                    for p in 0.25 0.5
+                    for p in 0.1 0.5 1.0
                     do
                         echo "N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.};
                         rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}"/validation.txt";
