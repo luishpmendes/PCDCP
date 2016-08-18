b1c402de0a201d14aa5ca8697c5e0b7c05e9419d
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-08-17 21:55:21 -0300

    bugfix

--- plotGenerator/generator2.plt
+++ plotGenerator/generator2.plt
@@ -4,9 +4,9 @@ unset key
 set size ratio -1 1,1
 set xrange [0:1]
 set yrange [0:1]
-set output "../".path."/output/N".n."D".D."K".k."T".t."R".r."P".P."/graph.pdf"
-set title "Instancia do PCDCP (N = ".n.";D = ".d.";K = ".k.";T = ".t."; R = ".r."; P = ".p.")"
-vertices = "../".path."/output/N".n."D".D."K".k."T".t."R".r."P".P."/vertices.txt"
-edges = "../".path."/output/N".n."D".D."K".k."T".t."R".r."P".P."/edges.txt"
+set output "../".path."/output/N".n."D".D."K".k."T".t."P".P."/graph.pdf"
+set title "Instancia do PCDCP (N = ".n."; D = ".d."; K = ".k."; T = ".t."; P = ".p.")"
+vertices = "../".path."/output/N".n."D".D."K".k."T".t."P".P."/vertices.txt"
+edges = "../".path."/output/N".n."D".D."K".k."T".t."P".P."/edges.txt"
 plot vertices using ($1):($2):($3) with points pointtype 7 pointsize variable linecolor rgb "black", \
 	 edges with lines linetype 1 linewidth 1 linecolor rgb "forest-green"
--- plotGenerator/plotGenerator2.sh
+++ plotGenerator/plotGenerator2.sh
@@ -12,13 +12,13 @@ do
                 for p in 0.1 0.5 1.0
                 do
                     echo "N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.};
-                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}"/vertices.txt";
-                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}"/edges.txt";
-                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}"/solutionVertices.txt";
-                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}"/solutionEdges.txt";
-                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"R"$r"P"${p//.}"/graph.pdf";
-                    ./plotGenerator $path $n ${d//.} $k $t $r ${p//.};
-                    gnuplot -e "path = '${path}'; n='${n}'; d='${d}'; k='${k}'; t='${t}'; r='${r}'; p='${p}'; D='${d//.}'; P='${p//.}';" generator2.plt;
+                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/vertices.txt";
+                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/edges.txt";
+                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/solutionVertices.txt";
+                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/solutionEdges.txt";
+                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/graph.pdf";
+                    ./plotGenerator $path $n ${d//.} $k $t ${p//.};
+                    gnuplot -e "path = '${path}'; n='${n}'; d='${d}'; k='${k}'; t='${t}'; p='${p}'; D='${d//.}'; P='${p//.}';" generator2.plt;
                 done
             done
         done
