baa0d85b21000424ac6bcb529db9246d334dbea0
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-08-30 15:21:59 -0300

    .

--- solutionValidator/solutionValidator.cpp
+++ solutionValidator/solutionValidator.cpp
@@ -7,10 +7,17 @@
 #include <sstream>
 #include <set>
 #include <map>
+#include <list>
+#include <queue>
+
+#ifndef INFINITE
+#define INFINITE 15 << 25
+#endif
 
 using namespace std;
 
 typedef long int ulint;
+typedef vector < vector <ulint> > matrix;
 
 string itos(ulint i) {
     stringstream s;
@@ -18,30 +25,68 @@ string itos(ulint i) {
     return s.str();
 }
 
+vector < set <ulint> > neighbourhoods (matrix W, ulint k) {
+    vector < set <ulint> > result (W.size());
+/*
+    matrix D = matrix (W.size(), vector <ulint> (W.size(), INFINITE));
+
+    for (ulint i = 0; i < W.size(); i++) {
+        for (ulint j = 0; j < W.size(); j++) {
+            D[i][j] = W[i][j];
+        }
+    }
+
+    for (ulint l = 0; l < W.size(); l++) {
+        for (ulint i = 0; i < W.size(); i++) {
+            for (ulint j = 0; j < W.size(); j++) {
+                if (D[i][j] > D[i][l] + D[l][j]) {
+                    D[i][j] = D[i][l] + D[l][j];
+                }
+            }
+        }
+    }
+
+    for (ulint u = 0; u < W.size(); u++) {
+        for (ulint v = 0; v < W[u].size(); v++) {
+            if (D[u][v] <= k) {
+                result[u].insert(v);
+            }
+        }
+    }
+*/
+    for (ulint u = 0; u < (ulint) W.size(); u++) {
+        for (ulint v = 0; v < (ulint) W[u].size(); v++) {
+            if (W[u][v] <= k) {
+                result[u].insert(v);
+            }
+        }
+    }
+    return result;
+}
+
 int main (int argc, char * argv[]) {
-    string path, N, D, K, T, R, P;
+    string path, N, D, K, T, P;
 
     cout << argc << endl;
 
-    if (argc == 8) {
+    if (argc == 7) {
         path = string(argv[1]);
         N = string(argv[2]);
         D = string(argv[3]);
         K = string(argv[4]);
         T = string(argv[5]);
-        R = string(argv[6]);
-        P = string(argv[7]);
+        P = string(argv[6]);
     } else {
-        cin >> path >> N >> D >> K >> T >> R;
+        cin >> path >> N >> D >> K >> T >> P;
     }
 
-    ifstream inputFile ("../input/instanceN" + N + "D" + D + "K" + K + "T" + T + "R" + R + "P" + P + ".in");
-    ifstream resultFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "R" + R + "P" + P + "/result.out");
+    ifstream inputFile ("../input/instanceN" + N + "D" + D + "K" + K + "T" + T + "P" + P + ".in");
+    ifstream resultFile ("../" + path + "/output/N" + N + "D" + D + "K" + K + "T" + T + "P" + P + "/result.out");
 
     if (inputFile.is_open() && resultFile.is_open()) {
-        ulint n, mComplete, m, k, t, r, root;
+        ulint n, mComplete, m, k, t, root;
         double d, p;
-        inputFile >> n >> d >> k >> t >> r >> p >> mComplete >> m >> root;
+        inputFile >> n >> d >> k >> t >> p >> mComplete >> m >> root;
 
         if (N == itos(n)) {
             cout << "N == n : OK" << endl;
@@ -55,35 +100,35 @@ int main (int argc, char * argv[]) {
             cout << "K == k : ERROR" << endl;
         }
 
-        if (R == itos(r)) {
-            cout << "R == r : OK" << endl;
-        } else {
-            cout << "R == r : ERROR" << endl;
-        }
-
-        vector < pair <double, double> > verticesCoordinates (n, make_pair(0, 0));
-        vector <ulint> penalties (n, 0);
+        vector < pair < pair <double, double>, ulint > > vertices (n, make_pair(make_pair(0, 0), 0));
         // reading vertices' coordinates and penalty
         for (ulint v = 0; v < n; v++) {
-            inputFile >> verticesCoordinates[v].first;
-            inputFile >> verticesCoordinates[v].second;
-            inputFile >> penalties[v];
+            inputFile >> vertices[v].first.first;
+            inputFile >> vertices[v].first.second;
+            inputFile >> vertices[v].second;
+        }
+
+        matrix W (n, vector <ulint> (n, INFINITE)); // adjacency matrix for the complete graph
+        for (ulint i = 0; i < n; i++) {
+            W[i][i] = 0;
         }
 
         // reading (and ignoring) the complete graph's edges
         for (ulint j = 0; j < mComplete; j++) {
             ulint u, v, w;
             inputFile >> u >> v >> w;
+            W[u][v] = w;
+            W[v][u] = w;
         }
 
         vector < pair <ulint, ulint> > edges (m, make_pair(0, 0));
         map < pair <ulint, ulint>, ulint > weights;
-        // reading the graph's edges and printing its coordinates
+        // reading the graph's edges and its weights
         for (ulint e = 0; e < m; e++) {
-            inputFile >> edges[e].first;
-            inputFile >> edges[e].second;
-            ulint w;
-            inputFile >> w;
+            ulint u, v, w;
+            inputFile >> u >> v >> w;
+            edges[e].first = u;
+            edges[e].second = v;
             weights[edges[e]] = w;
         }
 
@@ -107,6 +152,7 @@ int main (int argc, char * argv[]) {
         // reading the solution's vertices
         for (ulint v = 0; v < nSolution; v++) {
             resultFile >> solutionVertices[v];
+            // check if the solution vertice is a valid vertice
             if (solutionVertices[v] < 0 || solutionVertices[v] >= n) {
                 solutionVerticesInvalid.push_back(solutionVertices[v]);
             }
@@ -128,6 +174,7 @@ int main (int argc, char * argv[]) {
         for (ulint e = 0; e < mSolution; e++) {
             resultFile >> solutionEdges[e].first;
             resultFile >> solutionEdges[e].second;
+            // check if the ends of the solution edge are solution vertices
             if (setSolutionVertices.find(solutionEdges[e].first) == setSolutionVertices.end() 
             || setSolutionVertices.find(solutionEdges[e].second) == setSolutionVertices.end()) {
                 solutionEdgesInvalid.push_back(solutionEdges[e]);
@@ -143,6 +190,7 @@ int main (int argc, char * argv[]) {
             cout << endl;
         }
 
+        // compute solution cost
         ulint cost = 0;
         for (ulint e = 0; e < mSolution; e++) {
             cost += weights[solutionEdges[e]];
@@ -150,20 +198,123 @@ int main (int argc, char * argv[]) {
 
         ulint allPenalties = 0;
         for (ulint v = 0; v < n; v++) {
-            allPenalties += penalties[v];
+            allPenalties += vertices[v].second;
         }
 
         ulint chosenPenalties = 0;
         for (ulint v = 0; v < nSolution; v++) {
-            chosenPenalties += penalties[solutionVertices[v]];
+            chosenPenalties += vertices[solutionVertices[v]].second;
         }
 
         cost += (allPenalties - chosenPenalties);
 
+        // check if solution cost is correct
         if (costSolution == cost) {
             cout << "Solution Cost : OK" << endl;
         } else {
             cout << "Solution Cost : ERROR" << endl;
+        }
+
+        vector <ulint> verticesInSolution (n, 0);
+
+        for (ulint i = 0; i < nSolution; i++) {
+            ulint v = solutionVertices[i];
+            verticesInSolution[v] = 1;
+        }
+
+        vector < list < pair <ulint, ulint> > > adj (n);
+
+        for (ulint e = 0; e < mSolution; e++) {
+            ulint u, v, w;
+            u = solutionEdges[e].first;
+            v = solutionEdges[e].second;
+            w = weights[solutionEdges[e]];
+            adj[u].push_back(make_pair(v, w));
+            adj[v].push_back(make_pair(u, w));
+        }
+
+        // check if all vertices are covered
+        vector < set <ulint> > Ns = neighbourhoods (W, k);
+
+        set <ulint> uncoveredVertices;
+        for (ulint u = 0; u < n; u++) {
+            if (verticesInSolution[u] == 0) {
+                int flag = 0;
+                for (ulint i = 0; i < nSolution && flag == 0; i++) {
+                    ulint v = solutionVertices[i];
+                    if (Ns[v].find(u) != Ns[v].end()) {
+                        flag = 1;
+                    }
+                }
+                if (flag == 0) {
+                    uncoveredVertices.insert(u);
+                }
+            }
+        }
+
+        if (uncoveredVertices.size() <= 0) {
+            cout << "Cover : OK" << endl;
+        } else {
+            cout << "Cover : Error - Uncovered vertices: ";
+            for (set <ulint> :: iterator it = uncoveredVertices.begin(); it != uncoveredVertices.end(); it++) {
+                ulint u = *it;
+                cout << u << " ";
+            }
+            cout << endl;
+        }
+
+        vector <ulint> verticesInMainCycle (n, 0);
+
+        queue <ulint> Q;
+
+        Q.push(root);
+
+        while (!Q.empty()) {
+            ulint u = Q.front();
+            Q.pop();
+
+            for (list < pair <ulint, ulint> > :: iterator it = adj[u].begin(); it != adj[u].end(); it++) {
+                ulint v = it->first;
+                if (verticesInMainCycle[v] == 0) {
+                    verticesInMainCycle[v] = 1;
+                    Q.push(v);
+                }
+            }
+        }
+
+        vector <ulint> solutionVerticesNotInMainCycle;
+        vector <ulint> nonSolutionVerticesInMainCycle;
+
+        for (int v = 0; v < n; v++) {
+            if (verticesInSolution[v] == 1) {
+                if (verticesInMainCycle[v] == 0) {
+                    solutionVerticesNotInMainCycle.push_back(v);
+                }
+            } else {
+                if (verticesInMainCycle[v] == 1) {
+                    nonSolutionVerticesInMainCycle.push_back(v);
+                }
+            }
+        }
+
+        if (solutionVerticesNotInMainCycle.size() <= 0) {
+            cout << "Solution Vertice Not in Main Cycle: OK" << endl;
+        } else {
+            cout << "Solution Vertice Not in Main Cycle: Error - ";
+            for (int i = 0; i < solutionVerticesNotInMainCycle.size() - 1; i++) {
+                cout << solutionVerticesNotInMainCycle[i] << " ";
+            }
+            cout << solutionVerticesNotInMainCycle[solutionVerticesNotInMainCycle.size() - 1] << endl;
+        }
+
+        if (nonSolutionVerticesInMainCycle.size() <= 0) {
+            cout << "Non Solution Vertice in Main Cycle: OK" << endl;
+        } else {
+            cout << "Non Solution Vertice in Main Cycle: Error - ";
+            for (int i = 0; i < nonSolutionVerticesInMainCycle.size() - 1; i++) {
+                cout << nonSolutionVerticesInMainCycle[i] << " ";
+            }
+            cout << nonSolutionVerticesInMainCycle[nonSolutionVerticesInMainCycle.size() - 1] << endl;
         }
     }
     return 0;
