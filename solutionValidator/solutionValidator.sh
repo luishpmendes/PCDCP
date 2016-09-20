714fdf6fa4b53848646a2bcbce407d08836f9cb4
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-09-20 12:27:03 -0300

    nova valicacao

--- solutionValidator/solutionValidator.sh
+++ solutionValidator/solutionValidator.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 make solutionValidator;
-path="linearProgram";
-for n in 10 20 50 100
+for path in "linearProgram" "twoPhases"
 do
-    for d in 0.3 0.5 0.7
+    for n in 10 20 50 100
     do
-        for k in 0 5 10 20
+        for d in 0.3 0.5 0.7
         do
-            for t in 0 1
+            for k in 0 5 10 20
             do
-                for p in 0.1 0.5 1.0
+                for t in 0 1
                 do
-                    echo "N"$n"D"${d//.}"K"$k"T"$t"P"${p//.};
-                    rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/validation.txt";
-                    ./solutionValidator $path $n ${d//.} $k $t ${p//.} > "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/validation.txt";
+                    for p in 0.1 0.5 1.0
+                    do
+                        echo "N"$n"D"${d//.}"K"$k"T"$t"P"${p//.};
+                        rm -f "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/validation.txt";
+                        ./solutionValidator $path $n ${d//.} $k $t ${p//.} > "../"$path"/output/N"$n"D"${d//.}"K"$k"T"$t"P"${p//.}"/validation.txt";
+                    done
                 done
             done
         done
     done
 done
 make validationAggregator;
-./validationAggregator $path > "../"$path"/output/validation.txt";
+for path in "linearProgram" "twoPhases"
+do
+    ./validationAggregator $path > "../"$path"/output/validation.txt";
+done
 make clean;
