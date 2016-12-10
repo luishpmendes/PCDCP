3a2f2e9ef664c00de4778447b7dcf72854f34a34
Luis H. P. Mendes <luishpmendes@gmail.com>
2016-12-10 19:57:49 -0200

    linear program

--- linearProgram/solutionAggregator.cpp
+++ linearProgram/solutionAggregator.cpp
@@ -2,75 +2,96 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
 typedef unsigned long int ulint;
 
 int main (int argc, char * argv[]) {
-    vector <string> N;
-    N.push_back("10");
-    N.push_back("20");
-    N.push_back("50");
-    N.push_back("100");
-    N.push_back("250");
-    vector <string> D;
-    D.push_back("03");
-    D.push_back("05");
-    D.push_back("07");
-    vector <string> K;
-    K.push_back("0");
-    K.push_back("5");
-    K.push_back("10");
-    K.push_back("20");
-    vector <string> T;
-    T.push_back("0");
-    T.push_back("1");
-    vector <string> P;
-    P.push_back("01");
-    P.push_back("05");
-    P.push_back("10");
-    vector <string> I;
-    I.push_back("0");
-    I.push_back("1");
-    I.push_back("2");
-    I.push_back("3");
-    I.push_back("4");
-    I.push_back("5");
-    I.push_back("6");
-    I.push_back("7");
-    I.push_back("8");
-    I.push_back("9");
+    vector <int> vN;
+    vN.push_back(10);
+    vN.push_back(20);
+    vN.push_back(50);
+    vN.push_back(100);
+    vN.push_back(250);
+    vector <double> vD;
+    vD.push_back(0.3);
+    vD.push_back(0.5);
+    vD.push_back(0.7);
+    vector <int> vK;
+    vK.push_back(0);
+    vK.push_back(5);
+    vK.push_back(10);
+    vK.push_back(20);
+    vector <int> vT;
+    vT.push_back(0);
+    vT.push_back(1);
+    vector <double> vP;
+    vP.push_back(0.1);
+    vP.push_back(0.5);
+    vP.push_back(1.0);
+    vector <int> vI;
+    vI.push_back(0);
+    vI.push_back(1);
+    vI.push_back(2);
+    vI.push_back(3);
+    vI.push_back(4);
+    vI.push_back(5);
+    vI.push_back(6);
+    vI.push_back(7);
+    vI.push_back(8);
+    vI.push_back(9);
 
     cout << "n,d,k,t,p,i,objVal,gap,elapsedTime" << endl;
 
-    for (vector <string> :: iterator itN = N.begin(); itN != N.end(); itN++) {
-        string n = *itN;
-        for (vector <string> :: iterator itD = D.begin(); itD != D.end(); itD++) {
-            string d = *itD;
-            for (vector <string> :: iterator itK = K.begin(); itK != K.end(); itK++) {
-                string k = *itK;
-                for (vector <string> :: iterator itT = T.begin(); itT != T.end(); itT++) {
-                    string t = *itT;
-                    for (vector <string> :: iterator itP = P.begin(); itP != P.end(); itP++) {
-                        string p = *itP;
-                        for (vector <string> :: iterator itI = I.begin(); itI != I.end(); itI++) {
-                            string i = *itI;
+    for (vector <int> :: iterator itN = vN.begin(); itN != vN.end(); itN++) {
+        int n = *itN;
+        stringstream ssN;
+        ssN << n;
+        string N = ssN.str();
+        for (vector <double> :: iterator itD = vD.begin(); itD != vD.end(); itD++) {
+            double d = *itD;
+            stringstream ssD;
+            ssD << fixed << setprecision(1) << d;
+            string D = ssD.str();
+            for (vector <int> :: iterator itK = vK.begin(); itK != vK.end(); itK++) {
+                int k = *itK;
+                stringstream ssK;
+                ssK << k;
+                string K = ssK.str();
+                for (vector <int> :: iterator itT = vT.begin(); itT != vT.end(); itT++) {
+                    int t = *itT;
+                    stringstream ssT;
+                    ssK << t;
+                    string T = ssT.str();
+                    for (vector <double> :: iterator itP = vP.begin(); itP != vP.end(); itP++) {
+                        double p = *itP;
+                        stringstream ssP;
+                        ssP << fixed << setprecision(1) << p;
+                        string P = ssP.str();
+                        for (vector <int> :: iterator itI = vI.begin(); itI != vI.end(); itI++) {
+                            int i = *itI;
+                            stringstream ssI;
+                            ssK << i;
+                            string I = ssI.str();
 
                             double objVal = 0.0;
-                            ifstream objValFile ("./output/N" + n + "D" + d + "K" + k + "T" + t + "P" + p + "I" + i + "/objVal.txt");
+                            ifstream objValFile ("./output/N" + N + "D" + D + "K" + K + "T" + T + "P" + P + "I" + I + "/objVal.txt");
                             if (objValFile.is_open()) {
                                 objValFile >> objVal;
                             }
 
                             double gap = 0.0;
-                            ifstream gapFile ("./output/N" + n + "D" + d + "K" + k + "T" + t + "P" + p + "I" + i + "/gap.txt");
+                            ifstream gapFile ("./output/N" + N + "D" + D + "K" + K + "T" + T + "P" + P + "I" + I + "/gap.txt");
                             if (gapFile.is_open()) {
                                 gapFile >> gap;
                             }
 
                             ulint elapsedTime = 0.0;
-                            ifstream elapsedTimeFile ("./output/N" + n + "D" + d + "K" + k + "T" + t + "P" + p + "I" + i + "/elapsedTime.txt");
+                            ifstream elapsedTimeFile ("./output/N" + N + "D" + D + "K" + K + "T" + T + "P" + P + "I" + I + "/elapsedTime.txt");
                             if (elapsedTimeFile.is_open()) {
                                 elapsedTimeFile >> elapsedTime;
                             }
