bd9c5f74c4d32b32ffbad85350b32e58f7596633
Luis H. P. Mendes <luishpmendes@gmail.com>
2017-03-15 12:31:44 -0300

    novos parametros

--- instanceGenerator/instanceGenerator.sh
+++ instanceGenerator/instanceGenerator.sh
@@ -2,13 +2,13 @@
 rm -rf ../input;
 mkdir -p ../input;
 make instanceGenerator;
-for n in 10 20 50 100
+for n in 50 100 200
 do
     for d in 0.3 0.5 0.7
     do
         for k in 0 10 20
         do
-            for t in 0 1
+            for t in 0 1 2
             do
                 for i in 0
                 do
