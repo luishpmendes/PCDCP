d14f4f44f3c4aed018dcefeecd2627b103ea4317
Luis H. P. Mendes <luishpmendes@gmail.com>
2017-03-18 22:27:00 -0300

    .

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd linearProgram;
+sh ./linearProgram.sh;
+cd ..;
+cd twoPhases;
+sh ./twoPhases.sh;
+cd ..;
+cd grasp;
+sh ./grasp.sh;
+cd ..;
+cd geneticAlgorithm;
+sh ./geneticAlgorithm.sh;
+cd ..;
